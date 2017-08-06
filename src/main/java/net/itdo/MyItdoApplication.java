f9b126640736b818095d5fe504b4f6f48394e306
LAPTOP-7TE5K6VH\DoHyun <clover349@gmail.com>
2017-08-06 14:54:51 +0900

    [2017-08-06]최초생성

---
+++ src/main/java/net/itdo/MyItdoApplication.java
@@ -0,0 +1,12 @@
+package net.itdo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyItdoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyItdoApplication.class, args);
+	}
+}
