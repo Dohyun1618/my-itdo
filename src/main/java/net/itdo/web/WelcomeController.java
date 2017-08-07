928c5f82433c1a317d684a7e254179b5b4b2cfdb
LAPTOP-7TE5K6VH\DoHyun <clover349@gmail.com>
2017-08-08 00:11:25 +0900

    helloworld

---
+++ src/main/java/net/itdo/web/WelcomeController.java
@@ -0,0 +1,21 @@
+package net.itdo.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+	
+	private static final Logger log = LoggerFactory.getLogger(WelcomeController.class);
+
+	@GetMapping("/helloworld")
+	public String welcome(String email, Model model) {
+		log.debug(">>>>>>>>> email : {}" , email);
+		model.addAttribute("email", email);
+		return "welcome";
+	}
+
+}
